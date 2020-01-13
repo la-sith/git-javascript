29f54f2f2fce035941a37bbf3e9050c2de4b26c5
Lasith <wije0019@algonquinlive.com>
2020-01-13 15:36:43 -0500

    Created Index.html and scripts,js . Added values to both files.

---
+++ scripts.js
@@ -0,0 +1,15 @@
+let name = 'Lasith'
+console.log(` Hello, My name is ${name}. `)
+
+let number = 3
+let product = number * 2
+
+console.log(`The number ${number} multiplied by 2 is ${number * 2}.`)
+console.log(`The result ${product} multiplied by 5 is ${product * 5}.`)
+console.log(`The result 30 divided by ${number} is ${(product * 5) / number}.`)
+
+let finalValue = (product * 5) / number
+console.log(`The result 10 minus by 7 is ${finalValue - 7}.`)
+
+
+
